5e0944d6df302e9761c0b4bc28fa1efca7e77704
Chaliotis Anargyros <anargyros@chaliotis.com>
2024-03-25 14:56:01 +0200

    corrected a typo

--- django_portofolio/schema.py
+++ django_portofolio/schema.py
@@ -1,9 +1,9 @@
 # Import the necessary modules from graphene and the schema from the 'app' module
 import graphene
-import app.schema
+import app_portofolio.schema
 
 # Define a Query class that inherits from the Query class defined in app.schema and graphene's ObjectType
-class Query(app.schema.Query, graphene.ObjectType):
+class Query(app_portofolio.schema.Query, graphene.ObjectType):
     pass
 
 # Define a GraphQL schema with the above Query class as its query root
