fb1a79d6190f9734ae37275a9859d294d23b08b8
Chaliotis Anargyros <anargyros@chaliotis.com>
2024-03-25 14:47:55 +0200

    initialize app portofolio signals

--- app_portofolio/apps.py
+++ app_portofolio/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AppPortofolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_portofolio'
+
+    # Defining the ready method. This method is called once Django has carried out initialization tasks.
+    def ready(self):
+        # Importing the signals module from the app. This is typically where signal handlers are connected.
+        # By doing this here, we ensure that our signal handlers are connected when the app is ready.
+        import app_portofolio.signals
